40d225703ff99fca190798316924b93419953811
Vu Thuy <thuy.vu@bluebottle.digital>
2023-06-04 10:07:34 +0700

    Add python 123456

--- test_index.py
+++ test_index.py
@@ -1,23 +1,27 @@
-import pytest
+# import pytest
 
-from src.dtos.ISayHelloDto import ISayHelloDto
-from src.index import root, say_hello, hello_message
-
-
-@pytest.mark.asyncio
-async def test_root():
-    result = await root()
-    assert result == {'message': 'Hello World'}
+# from src.dtos.ISayHelloDto import ISayHelloDto
+# from src.index import root, say_hello, hello_message
 
 
-@pytest.mark.asyncio
-async def test_say_hello():
-    result = await say_hello("John")
-    assert result == {'message': 'Hello John'}
+# @pytest.mark.asyncio
+# async def test_root():
+#     result = await root()
+#     assert result == {'message': 'Hello World'}
 
 
-@pytest.mark.asyncio
-async def test_hello_message():
-    dto = ISayHelloDto(message="Alice")
-    result = await hello_message(dto)
-    assert result == {'message': 'Hello Alice'}
+# @pytest.mark.asyncio
+# async def test_say_hello():
+#     result = await say_hello("John")
+#     assert result == {'message': 'Hello John'}
+
+
+# @pytest.mark.asyncio
+# async def test_hello_message():
+#     dto = ISayHelloDto(message="Alice")
+#     result = await hello_message(dto)
+#     assert result == {'message': 'Hello Alice'}
+from vnstock import *
+
+df = get_index_series(index_code='VNINDEX', time_range='OneYear')
+print(df)
