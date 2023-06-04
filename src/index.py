01b215d1966881ba199a36c8d0100324148224e7
Vu Thuy <thuy.vu@bluebottle.digital>
2023-06-04 09:20:58 +0700

    Add python 123

--- src/index.py
+++ src/index.py
@@ -1,5 +1,6 @@
 # from requests
-from flask import request
+# from flask import request
+import requests
 from fastapi import FastAPI
 from src.dtos.ISayHelloDto import ISayHelloDto
 
@@ -30,7 +31,7 @@ headers = {
 async def root():
     url = f"https://fiin-market.ssi.com.vn/MarketInDepth/GetIndexSeries?language=vi&ComGroupCode=VNINDEX&TimeRange=OneYear&id=1"
     payload={}
-    response =  request("GET", url, headers=headers, data=payload)
+    response =  requests.request("GET", url, headers=headers, data=payload)
     return response.json()
 
 
