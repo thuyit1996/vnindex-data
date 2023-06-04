5bae69580b32fd9b706e3a3e785ea90fed9b093b
Vu Thuy <thuy.vu@bluebottle.digital>
2023-06-04 09:27:51 +0700

    Add python 1234

--- src/index.py
+++ src/index.py
@@ -7,31 +7,30 @@ from src.dtos.ISayHelloDto import ISayHelloDto
 app = FastAPI()
 
 headers = {
-        'Connection': 'keep-alive',
-        'sec-ch-ua': '"Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
-        'DNT': '1',
-        'sec-ch-ua-mobile': '?0',
-        'X-Fiin-Key': 'KEY',
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'X-Fiin-User-ID': 'ID',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
-        'X-Fiin-Seed': 'SEED',
-        'sec-ch-ua-platform': 'Windows',
-        'Origin': 'https://iboard.ssi.com.vn',
-        'Sec-Fetch-Site': 'same-site',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Dest': 'empty',
-        'Referer': 'https://iboard.ssi.com.vn/',
-        'Accept-Language': 'en-US,en;q=0.9,vi-VN;q=0.8,vi;q=0.7'
-        }
+    'Connection': 'keep-alive',
+    'sec-ch-ua': '"Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
+    'DNT': '1',
+    'sec-ch-ua-mobile': '?0',
+    'X-Fiin-Key': 'KEY',
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'X-Fiin-User-ID': 'ID',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+    'X-Fiin-Seed': 'SEED',
+    'sec-ch-ua-platform': 'Windows',
+    'Origin': 'https://iboard.ssi.com.vn',
+    'Sec-Fetch-Site': 'same-site',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Dest': 'empty',
+    'Referer': 'https://iboard.ssi.com.vn/',
+    'Accept-Language': 'en-US,en;q=0.9,vi-VN;q=0.8,vi;q=0.7'
+}
 
 
 @app.get("/")
 async def root():
-    url = f"https://fiin-market.ssi.com.vn/MarketInDepth/GetIndexSeries?language=vi&ComGroupCode=VNINDEX&TimeRange=OneYear&id=1"
-    payload={}
-    response =  requests.request("GET", url, headers=headers, data=payload)
+    url = "https://fiin-market.ssi.com.vn/MarketInDepth/GetIndexSeries?language=vi&ComGroupCode=VNINDEX&TimeRange=OneYear&id=1"
+    response = requests.get(url, headers=headers)
     return response
 
 
